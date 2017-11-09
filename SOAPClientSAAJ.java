67e3bb2d0e8d178f9e425b8bef1dd9d5830bf7f2
kiranyadav245 <kiranyadav245@gmail.com>
2017-11-09 23:45:08 +0530

    soap - sending  xml file as request and response operaions

---
+++ SOAPClientSAAJ.java
@@ -0,0 +1,155 @@
+package test;
+import javax.xml.soap.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+
+public class SOAPClientSAAJ {
+
+    // SAAJ - SOAP Client Testing
+    public static void main(String args[]) {
+        /*
+            The example below requests from the Web Service at:
+             http://www.webservicex.net/uszip.asmx?op=GetInfoByCity
+
+
+            To call other WS, change the parameters below, which are:
+             - the SOAP Endpoint URL (that is, where the service is responding from)
+             - the SOAP Action
+
+            Also change the contents of the method createSoapEnvelope() in this class. It constructs
+             the inner part of the SOAP envelope that is actually sent.
+         */
+        String soapEndpointUrl = "http://www.webservicex.net/uszip.asmx";
+        String soapAction = "http://www.webserviceX.NET/GetInfoByCity";
+
+        callSoapWebService(soapEndpointUrl, soapAction);
+    }
+
+    private static void createSoapEnvelope(SOAPMessage soapMessage) throws SOAPException {
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+
+        String myNamespace = "myNamespace";
+        String myNamespaceURI = "http://www.webserviceX.NET";
+
+        // SOAP Envelope
+        SOAPEnvelope envelope = soapPart.getEnvelope();
+        envelope.addNamespaceDeclaration(myNamespace, myNamespaceURI);
+
+            /*
+            Constructed SOAP Request Message:
+            <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:myNamespace="http://www.webserviceX.NET">
+                <SOAP-ENV:Header/>
+                <SOAP-ENV:Body>
+                    <myNamespace:GetInfoByCity>
+                        <myNamespace:USCity>New York</myNamespace:USCity>
+                    </myNamespace:GetInfoByCity>
+                </SOAP-ENV:Body>
+            </SOAP-ENV:Envelope>
+            */
+
+        // SOAP Body
+        SOAPBody soapBody = envelope.getBody();
+        SOAPElement soapBodyElem = soapBody.addChildElement("GetInfoByCity", myNamespace);
+        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("USCity", myNamespace);
+        soapBodyElem1.addTextNode("New York");
+    }
+
+    private static void callSoapWebService(String soapEndpointUrl, String soapAction) {
+        try {
+            // Create SOAP Connection
+            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+            // Send SOAP Message to SOAP Server
+            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(soapAction), soapEndpointUrl);
+            System.out.println("soapResponse");
+            soapResponse.writeTo(System.out);
+            System.out.println();
+            // Print the SOAP Response
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            soapResponse.writeTo(out);
+            String RawSoapResponse = new String(out.toByteArray());
+            System.out.println("Response SOAP RAW Message:" );
+            System.out.println(RawSoapResponse);
+            Document doc = convertStringToDocument(RawSoapResponse);
+            
+            String soapResponseInString = convertDocumentToString(doc);
+            System.out.println("soapResponseInString ::::::" +soapResponseInString);
+            
+
+            soapConnection.close();
+        } catch (Exception e) {
+            System.err.println("\nError occurred while sending SOAP Request to Server!\nMake sure you have the correct endpoint URL and SOAPAction!\n");
+            e.printStackTrace();
+        }
+    }
+
+    private static SOAPMessage createSOAPRequest(String soapAction) throws Exception {
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+
+        createSoapEnvelope(soapMessage);
+
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", soapAction);
+
+        soapMessage.saveChanges();
+
+        /* Print the request message, just for debugging purposes */
+        System.out.println("Request SOAP Message:");
+        soapMessage.writeTo(System.out);
+        System.out.println("\n");
+
+        return soapMessage;
+    }
+    
+    private static String convertDocumentToString(Document doc) {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer;
+        try {
+            transformer = tf.newTransformer();
+            // below code to remove XML declaration
+            // transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(doc), new StreamResult(writer));
+            String output = writer.getBuffer().toString();
+            return output;
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+
+    private static Document convertStringToDocument(String xmlStr) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
+        DocumentBuilder builder;  
+        try  
+        {  
+            builder = factory.newDocumentBuilder();  
+            Document doc = builder.parse( new InputSource( new StringReader( xmlStr ) ) ); 
+            return doc;
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        } 
+        return null;
+    }
+
+}
+
+
